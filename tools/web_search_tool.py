db965a04216c86c08d780d7ec409b68085580c4e
ram_swaroop <ramswaroopbhakar14@gmail.com>
2025-06-25 00:10:42 +0530

    tool: update langchain tavily import

--- tools/web_search_tool.py
+++ tools/web_search_tool.py
@@ -1,5 +1,12 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+# from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
 import os
+from dotenv import load_dotenv
 
-# Tavily API Key is aleady set in the .env file
-web_search_tool = TavilySearchResults(k=3)
+load_dotenv()
+
+# TAVILY_API_KEY = os.getenv("TAVILY_API_KEY","Not Set")
+
+# print("TAVILY API Key:", TAVILY_API_KEY)
+# web_search_tool = TavilySearchResults(k=3)
+web_search_tool = TavilySearch(k=3)
